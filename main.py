2d0eab390a2aa3353dcf2e1859edf97700b3c6b2
Juan Jiménez <nefioxdev@gmail.com>
2021-12-10 14:36:42 -0600

    Query Parameters

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # Python
 from typing import Optional
+from fastapi.param_functions import Query
 
-# Pydantic
+# Pydantic (FastAPI funciona sobre Pydantic, es por eso que se pone abajo)
 from pydantic import BaseModel
 
 # FastAPI
 from fastapi import FastAPI
-from fastapi import Body
+from fastapi import Body, Query
 
 app = FastAPI()
 
@@ -27,3 +28,12 @@ def home():
 @app.post('/person/new')
 def create_person(person: Person = Body(...)):
     return person
+
+# Validaciones: Query Parameters
+@app.get('/person/detail')
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: Optional[str] = Query(...)
+    
+):
+    return {name: age}
